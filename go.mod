3887acc657004b7db3631a32fb0307c3a8822401
Antonio Martinez <jose.antonio.martinez.torres@intel.com>
2019-08-16 09:57:51 -0700

    Initial implementation

--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module github.impcloud.net/RSP-Inventory-Suite/go-odata
 go 1.12
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.3.3
 	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
+	github.com/jmoiron/sqlx v1.2.0
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/lib/pq v1.0.0
 	github.com/pkg/errors v0.8.1
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
-	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce // indirect
+	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
 	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- postgresql/postgresql.go
+++ postgresql/postgresql.go
@@ -1,179 +1,201 @@
 package postgresql
 
 import (
-	"encoding/hex"
 	"net/url"
+	"reflect"
+	"strconv"
 	"strings"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
 	"github.impcloud.net/RSP-Inventory-Suite/go-odata/parser"
-	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
 )
 
 // ErrInvalidInput Client errors
 var ErrInvalidInput = errors.New("odata syntax error")
 
-func ODataQuery(query url.Values, object interface{}, collection *mgo.Collection) error {
+var sqlOperators = map[string]string{
+	"eq": "=",
+	"nq": "!=",
+	"gt": ">",
+	"ge": ">=",
+	"lt": "<",
+	"le": "<=",
+}
+
+var valuesMap = make(map[string]interface{})
+
+// ODataSQLQuery builds a SQL like query based on OData 2.0 specification
+func ODataSQLQuery(query url.Values, table string, column string, db *sqlx.DB) (*sqlx.Rows, error) {
 
 	// Parse url values
 	queryMap, err := parser.ParseURLValues(query)
 	if err != nil {
-		return errors.Wrap(ErrInvalidInput, err.Error())
+		return nil, errors.Wrap(ErrInvalidInput, err.Error())
 	}
 
-	limit, _ := queryMap[parser.Top].(int)
-	skip, _ := queryMap[parser.Skip].(int)
+	var finalQuery strings.Builder
 
-	var filterClause string
+	// SELECT clause
+	finalQuery.WriteString(buildSelectClause(queryMap, column))
 
+	// FROM clause
+	finalQuery.WriteString(" FROM ")
+	finalQuery.WriteString(table)
+
+	// WHERE clause
 	if queryMap[parser.Filter] != nil {
+		finalQuery.WriteString(" WHERE ")
 		filterQuery, _ := queryMap[parser.Filter].(*parser.ParseNode)
-		var err error
-		filterClause, err = applyFilter(filterQuery)
+		filterClause, err := applyFilter(filterQuery, column)
 		if err != nil {
-			return errors.Wrap(ErrInvalidInput, err.Error())
+			return nil, errors.Wrap(ErrInvalidInput, err.Error())
 		}
+
+		finalQuery.WriteString(filterClause)
 	}
 
-	// Prepare Select 
-	var select string
-	if queryMap["$select"] != nil {
-		selectSlice := reflect.ValueOf(queryMap["$select"])
-		if selectSlice.Len() > 1 && selectSlice.Index(0).Interface().(string) != "*" {			
-			for i := 0; i < selectSlice.Len(); i++ {
-				fieldName := selectSlice.Index(i).Interface().(string)
-				selectMap[fieldName] = 1
-			}
-		}
+	// Limit & Offset
+	limit, existLimit := queryMap[parser.Top].(int)
+	skip, existSkip := queryMap[parser.Skip].(int)
+
+	if existLimit {
+		finalQuery.WriteString(" LIMIT ")
+		finalQuery.WriteString(strconv.Itoa(limit))
+		finalQuery.WriteString(" ")
 	}
 
+	if existSkip {
+		finalQuery.WriteString(" OFFSET ")
+		finalQuery.WriteString(strconv.Itoa(skip))
+		finalQuery.WriteString(" ")
+	}
+
+	queryString := finalQuery.String()
+	rows, err := db.NamedQuery(queryString, valuesMap)
+	if err != nil {
+		return nil, err
+	}
+
+	return rows, nil
 
 }
 
-func applyFilter(node *parser.ParseNode) (bson.M, error) {
+func buildSelectClause(queryMap map[string]interface{}, column string) string {
 
-	filter := make(bson.M)
+	// Select clause
+	// 'data' is the column name of the jsonb data
+	var selectClause strings.Builder
+	selectClause.WriteString("SELECT ")
+	if queryMap["$select"] != nil {
+		selectSlice := reflect.ValueOf(queryMap["$select"])
+		if selectSlice.Len() > 1 && selectSlice.Index(0).Interface().(string) != "*" {
+			for i := 0; i < selectSlice.Len(); i++ {
+				fieldName := selectSlice.Index(i).Interface().(string)
+				selectClause.WriteString(column)
+				selectClause.WriteString(" -> ")
+				selectClause.WriteString("'")
+				selectClause.WriteString(fieldName)
+				selectClause.WriteString("'")
+
+				selectClause.WriteString(" AS ")
+				selectClause.WriteString(fieldName)
+
+				if selectSlice.Len() > 1 {
+					selectClause.WriteString(",")
+				}
+			}
+		}
+	} else {
+		selectClause.WriteString(" * ")
+	}
+
+	return selectClause.String()
+
+}
+
+func applyFilter(node *parser.ParseNode, column string) (string, error) {
+
+	var filter strings.Builder
 
 	if _, ok := node.Token.Value.(string); ok {
 		switch node.Token.Value {
 
-		case "eq":
-			// Escape single quotes in the case of strings
-			if _, valueOk := node.Children[1].Token.Value.(string); valueOk {
-				node.Children[1].Token.Value = strings.Replace(node.Children[1].Token.Value.(string), "'", "", -1)
-			}
-			value := bson.M{"$" + node.Token.Value.(string): node.Children[1].Token.Value}
+		case "eq", "ne", "gt", "ge", "lt", "le":
 			if _, keyOk := node.Children[0].Token.Value.(string); !keyOk {
-				return nil, ErrInvalidInput
+				return "", ErrInvalidInput
 			}
-			filter[node.Children[0].Token.Value.(string)] = value
+			filter.WriteString(column)
+			filter.WriteString(" ->> ")
+			filter.WriteString("'")
+			filter.WriteString(node.Children[0].Token.Value.(string))
+			filter.WriteString("'")
 
-		case "ne":
-			// Escape single quotes in the case of strings
-			if _, valueOk := node.Children[1].Token.Value.(string); valueOk {
-				node.Children[1].Token.Value = strings.Replace(node.Children[1].Token.Value.(string), "'", "", -1)
-			}
-			value := bson.M{"$" + node.Token.Value.(string): node.Children[1].Token.Value}
-			if _, keyOk := node.Children[0].Token.Value.(string); !keyOk {
-				return nil, ErrInvalidInput
-			}
-			filter[node.Children[0].Token.Value.(string)] = value
-
-		case "gt":
-			var keyString string
-			if keyString, ok = node.Children[0].Token.Value.(string); !ok {
-				return nil, ErrInvalidInput
-			}
-
-			var value bson.M
-			if keyString == "_id" {
-				var idString string
-				if _, ok := node.Children[1].Token.Value.(string); ok {
-					idString = strings.Replace(node.Children[1].Token.Value.(string), "'", "", -1)
-				}
-				decodedString, err := hex.DecodeString(idString)
-				if err != nil || len(decodedString) != 12 {
-					return nil, ErrInvalidInput
-				}
-				value = bson.M{"$" + node.Token.Value.(string): bson.ObjectId(decodedString)}
+			if operator, ok := sqlOperators[node.Token.Value.(string)]; ok {
+				filter.WriteString(operator)
 			} else {
-				value = bson.M{"$" + node.Token.Value.(string): node.Children[1].Token.Value}
+				return "", ErrInvalidInput
 			}
-			filter[keyString] = value
-
-		case "ge":
-			value := bson.M{"$gte": node.Children[1].Token.Value}
-			if _, ok := node.Children[0].Token.Value.(string); !ok {
-				return nil, ErrInvalidInput
-			}
-			filter[node.Children[0].Token.Value.(string)] = value
-
-		case "lt":
-			value := bson.M{"$" + node.Token.Value.(string): node.Children[1].Token.Value}
-			if _, ok := node.Children[0].Token.Value.(string); !ok {
-				return nil, ErrInvalidInput
-			}
-			filter[node.Children[0].Token.Value.(string)] = value
-
-		case "le":
-			value := bson.M{"$lte": node.Children[1].Token.Value}
-			if _, ok := node.Children[0].Token.Value.(string); !ok {
-				return nil, ErrInvalidInput
-			}
-			filter[node.Children[0].Token.Value.(string)] = value
+			filter.WriteString(":")
+			filter.WriteString(node.Children[0].Token.Value.(string))
+			valuesMap[node.Children[0].Token.Value.(string)] = node.Children[0].Token.Value.(string)
 
 		case "and":
-			leftFilter, err := applyFilter(node.Children[0]) // Left children
+			leftFilter, err := applyFilter(node.Children[0], column) // Left children
 			if err != nil {
-				return nil, err
+				return "", err
 			}
-			rightFilter, _ := applyFilter(node.Children[1]) // Right children
+			rightFilter, _ := applyFilter(node.Children[1], column) // Right children
 			if err != nil {
-				return nil, err
+				return "", err
 			}
-			filter["$and"] = []bson.M{leftFilter, rightFilter}
+
+			filter.WriteString(leftFilter)
+			filter.WriteString(" AND ")
+			filter.WriteString(rightFilter)
 
 		case "or":
-			leftFilter, err := applyFilter(node.Children[0]) // Left children
+			leftFilter, err := applyFilter(node.Children[0], column) // Left children
 			if err != nil {
-				return nil, err
+				return "", err
 			}
-			rightFilter, err := applyFilter(node.Children[1]) // Right children
+			rightFilter, err := applyFilter(node.Children[1], column) // Right children
 			if err != nil {
-				return nil, err
+				return "", err
 			}
-			filter["$or"] = []bson.M{leftFilter, rightFilter}
+			filter.WriteString(leftFilter)
+			filter.WriteString(" OR ")
+			filter.WriteString(rightFilter)
 
-		//Functions
-		case "startswith":
-			if _, ok := node.Children[1].Token.Value.(string); !ok {
-				return nil, ErrInvalidInput
-			}
-			node.Children[1].Token.Value = strings.Replace(node.Children[1].Token.Value.(string), "'", "", -1)
-			//nolint: vet
-			value := bson.RegEx{"^" + node.Children[1].Token.Value.(string), "gi"}
-			filter[node.Children[0].Token.Value.(string)] = value
+			// //Functions
+			// case "startswith":
+			// 	if _, ok := node.Children[1].Token.Value.(string); !ok {
+			// 		return nil, ErrInvalidInput
+			// 	}
+			// 	node.Children[1].Token.Value = strings.Replace(node.Children[1].Token.Value.(string), "'", "", -1)
+			// 	//nolint: vet
+			// 	value := bson.RegEx{"^" + node.Children[1].Token.Value.(string), "gi"}
+			// 	filter[node.Children[0].Token.Value.(string)] = value
 
-		case "endswith":
-			if _, ok := node.Children[1].Token.Value.(string); !ok {
-				return nil, ErrInvalidInput
-			}
-			node.Children[1].Token.Value = strings.Replace(node.Children[1].Token.Value.(string), "'", "", -1)
-			//nolint: vet
-			value := bson.RegEx{node.Children[1].Token.Value.(string) + "$", "gi"}
-			filter[node.Children[0].Token.Value.(string)] = value
+			// case "endswith":
+			// 	if _, ok := node.Children[1].Token.Value.(string); !ok {
+			// 		return nil, ErrInvalidInput
+			// 	}
+			// 	node.Children[1].Token.Value = strings.Replace(node.Children[1].Token.Value.(string), "'", "", -1)
+			// 	//nolint: vet
+			// 	value := bson.RegEx{node.Children[1].Token.Value.(string) + "$", "gi"}
+			// 	filter[node.Children[0].Token.Value.(string)] = value
 
-		case "contains":
-			if _, ok := node.Children[1].Token.Value.(string); !ok {
-				return nil, ErrInvalidInput
-			}
-			node.Children[1].Token.Value = strings.Replace(node.Children[1].Token.Value.(string), "'", "", -1)
-			//nolint: vet
-			value := bson.RegEx{node.Children[1].Token.Value.(string), "gi"}
-			filter[node.Children[0].Token.Value.(string)] = value
+			// case "contains":
+			// 	if _, ok := node.Children[1].Token.Value.(string); !ok {
+			// 		return nil, ErrInvalidInput
+			// 	}
+			// 	node.Children[1].Token.Value = strings.Replace(node.Children[1].Token.Value.(string), "'", "", -1)
+			// 	//nolint: vet
+			// 	value := bson.RegEx{node.Children[1].Token.Value.(string), "gi"}
+			// 	filter[node.Children[0].Token.Value.(string)] = value
 
 		}
 	}
-	return filter, nil
+	return filter.String(), nil
 }
