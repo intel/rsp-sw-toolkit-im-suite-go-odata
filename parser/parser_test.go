b9be32560e117502fda567f1cd8507c8c9cd80a8
sean-williams <sean.williams@intel.com>
2019-12-10 12:07:42 -0700

    update copyright (#7)
    * update copyright
    
    * add apache license

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,20 +1,7 @@
-/*
- * INTEL CONFIDENTIAL
- * Copyright (2017) Intel Corporation.
- *
- * The source code contained or described herein and all documents related to the source code ("Material")
- * are owned by Intel Corporation or its suppliers or licensors. Title to the Material remains with
- * Intel Corporation or its suppliers and licensors. The Material may contain trade secrets and proprietary
- * and confidential information of Intel Corporation and its suppliers and licensors, and is protected by
- * worldwide copyright and trade secret laws and treaty provisions. No part of the Material may be used,
- * copied, reproduced, modified, published, uploaded, posted, transmitted, distributed, or disclosed in
- * any way without Intel/'s prior express written permission.
- * No license under any patent, copyright, trade secret or other intellectual property right is granted
- * to or conferred upon you by disclosure or delivery of the Materials, either expressly, by implication,
- * inducement, estoppel or otherwise. Any license under such intellectual property rights must be express
- * and approved by Intel in writing.
- * Unless otherwise agreed by Intel in writing, you may not remove or alter this notice or any other
- * notice embedded in Materials by Intel or Intel's suppliers or licensors in any way.
+/* Apache v2 license
+*  Copyright (C) <2019> Intel Corporation
+*
+*  SPDX-License-Identifier: Apache-2.0
  */
 
 package parser
@@ -409,16 +396,16 @@ func TestParseWithFilter(t *testing.T) {
 			"(SKU eq '123') and contains(epc_time, 0) and startswith(epc_code, '456') or " +
 			"endswith(upc_code, '789'))", true, nil}, // large valid filter case
 		{"_id gt '59a6fbaf22e60174f5107a9a' and upc_code eq 'val'", true, nil}, // paging with mongo id
-		{"gtin eq '123'", true, nil},                                           // key name with operator substring
-		{"name eq 'val')", false, errors.New("")},                              // bad parentheses
-		{"(name eq )", false, errors.New("")},                                  // missing value in equals operator
-		{"(name eq hello) and (name fakeop hello)", false, errors.New("")},     // bad operator
-		{"epc_item_type ne 0 and name", false, errors.New("")},                 // operators can't have a mix operators and literals
-		{"epc_item_type ne 0 and 0", false, errors.New("")},                    // // operators can't have a mix operators and literals
-		{"name and epc_item_type ne 0", false, errors.New("")},                 // // operators can't have a mix operators and literals
-		{"name eqs epc_item_type", false, errors.New("")},                      // typo operator
-		{"contains(and, epc_item_type)", false, errors.New("")},                // operator in function
-		{"", false, errors.New("")},                                            // empty string test
+		{"gtin eq '123'", true, nil},                                       // key name with operator substring
+		{"name eq 'val')", false, errors.New("")},                          // bad parentheses
+		{"(name eq )", false, errors.New("")},                              // missing value in equals operator
+		{"(name eq hello) and (name fakeop hello)", false, errors.New("")}, // bad operator
+		{"epc_item_type ne 0 and name", false, errors.New("")},             // operators can't have a mix operators and literals
+		{"epc_item_type ne 0 and 0", false, errors.New("")},                // // operators can't have a mix operators and literals
+		{"name and epc_item_type ne 0", false, errors.New("")},             // // operators can't have a mix operators and literals
+		{"name eqs epc_item_type", false, errors.New("")},                  // typo operator
+		{"contains(and, epc_item_type)", false, errors.New("")},            // operator in function
+		{"", false, errors.New("")},                                        // empty string test
 	}
 
 	for _, expectedVal := range filterTests {
--- postgresql/postgresql_test.go
+++ postgresql/postgresql_test.go
@@ -1,3 +1,9 @@
+/* Apache v2 license
+*  Copyright (C) <2019> Intel Corporation
+*
+*  SPDX-License-Identifier: Apache-2.0
+ */
+
 package postgresql
 
 import (
